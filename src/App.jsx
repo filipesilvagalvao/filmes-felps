4a8f267dca1f20578e60acb56f91dc1d15ebd537
filipe <felepesilvagalvao@gmail.com>
2025-08-28 18:00:50 -0300

    Componente Home.jsx add ao App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Header'
+import Home from './components/Home'
 import Slider from './components/Slider'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Slider />
+      <Home />
     </>
   )
 }
